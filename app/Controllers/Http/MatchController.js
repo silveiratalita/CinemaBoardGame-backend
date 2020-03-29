7cb06f0ac2e3850153801fd5b8f5cbc9710876d7
Gustavo Oliveira <gustavo@MacBook-Pro-15-de-Gustavo.local>
2020-03-29 10:18:12 -0300

    Finalizado Get em Matches

--- app/Controllers/Http/MatchController.js
+++ app/Controllers/Http/MatchController.js
@@ -44,42 +44,32 @@ class MatchController {
   async getMatches ({ request, response }) {
     try {
       const { userId } = request.all()
-      const matchesResult = await Database.raw(`select m.id, m."date", m.winner from users u2 inner join playermatches p2 on (u2.id = p2.user_id) inner join matches m on (p2.match_id = m.id) where u2.id = ${userId}`)
+      //busca os jogos do jogador
+      const matchesResult =
+        await Database
+          .from('users')
+          .innerJoin('playermatches', 'playermatches.user_id', 'users.id')
+          .innerJoin('matches', 'matches.id', 'playermatches.match_id')
+          .where('users.id', userId)
+          .select('matches.id', 'matches.date', 'matches.winner', 'room_name', 'number_of_rounds')
 
-
-
-      for (let i = 0; i < Object.keys(matchesResult.rows).length; i++) {
-        const m = Object.values(matchesResult.rows[i])
-        Logger.debug(m.id)
-        const players = this.getPlayersOfTheMatch(m.id)
-        Logger.debug(players)
+      //busca os jogadores de cada jogo e insere no objeto para retorno
+      for (let i = 0; i < matchesResult.length; i++) {
+        const m = matchesResult[i].id
+        const players = await Database
+          .select('users.id', 'users.username', 'users.email')
+          .from('users')
+          .innerJoin('playermatches', 'users.id', 'playermatches.user_id')
+          .where('playermatches.match_id', m)
+        //insere no objeto para retorno
+        matchesResult[i].players = players
       }
 
-      return response.send(matchesResult.rows)
+      return response.send(matchesResult)
     } catch (err) {
       console.log(err)
       return response.send(err)
     }
-  }
-
-  async getPlayersOfTheMatch (matchID) {
-    const playersOfTheMatchResult = []
-    const playersQuery = `select
-                         u2.username
-                       from
-                         users u2
-                       inner join playermatches p2 on
-                         (u2.id = p2.user_id)
-                       where
-                         p2.match_id = ${matchID}`
-
-    const playersResult = await Database.raw(playersQuery)
-
-    for (let j = 0; j < Object.keys(playersResult.rows).length; j++) {
-      playersOfTheMatchResult[j] = Object.values(playersResult.rows[j])
-    }
-
-    return playersOfTheMatchResult
   }
 }
 
